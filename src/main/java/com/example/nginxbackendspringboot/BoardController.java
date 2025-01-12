669245343cb18b3dac6201dd590afc2b6fa3156c
jaeseongDev <qkrwotjd1445@naver.com>
2025-01-12 13:45:13 +0900

    first commit

---
+++ src/main/java/com/example/nginxbackendspringboot/BoardController.java
@@ -0,0 +1,21 @@
+package com.example.nginxbackendspringboot;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("boards")
+public class BoardController {
+
+  @GetMapping()
+  public List<Board> getBoards() {
+    List<Board> boards = new ArrayList<>();
+    boards.add(new Board(1L, "제목1", "내용1"));
+    boards.add(new Board(2L, "제목2", "내용2"));
+    boards.add(new Board(3L, "제목3", "내용3"));
+    return boards;
+  }
+}
---
+++ src/main/java/com/example/nginxbackendspringboot/NginxBackendSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.nginxbackendspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NginxBackendSpringbootApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(NginxBackendSpringbootApplication.class, args);
+  }
+
+}
